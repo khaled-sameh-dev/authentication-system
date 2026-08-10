f7de87a46033e8a3a250522683d38ee189a3cd19
unknown <khaledsameh358@gmail.com>
2026-08-10 18:35:10 +0300

    add api url

--- client/src/config/env.config.ts
+++ client/src/config/env.config.ts
@@ -1,5 +1,5 @@
 export const ENV = {
   API_BASE_URL:
-    import.meta.env.VITE_API_BASE_URL || "http://localhost:5003/api/v1",
+    `${import.meta.env.VITE_API_BASE_URL}/api/v1` || "https://authentication-system-ru7o.vercel.app/register/api/v1",
   IS_DEV: import.meta.env.DEV,
 };
