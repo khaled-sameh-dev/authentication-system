19feb024369d8eb7cc62259a13cf8ae67d55569d
unknown <khaledsameh358@gmail.com>
2026-08-10 18:39:02 +0300

    modify for deployment

--- client/src/config/env.config.ts
+++ client/src/config/env.config.ts
@@ -1,5 +1,6 @@
 export const ENV = {
   API_BASE_URL:
-    `${import.meta.env.VITE_API_BASE_URL}/api/v1` || "https://authentication-system-ru7o.vercel.app/register/api/v1",
+    `${import.meta.env.VITE_API_BASE_URL}/api/v1` ||
+    "https://authentication-system-one-orpin.vercel.app/register/api/v1",
   IS_DEV: import.meta.env.DEV,
 };
