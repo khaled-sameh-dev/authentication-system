3f6714919b36b8a864d5466c5579adf1a8da04de
unknown <khaledsameh358@gmail.com>
2026-08-13 15:43:22 +0300

    modify env

--- client/src/config/env.config.ts
+++ client/src/config/env.config.ts
@@ -1,6 +1,10 @@
+const rawBaseUrl =
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://authentication-system-production-40b5.up.railway.app";
+
+const cleanBaseUrl = rawBaseUrl.replace(/\/$/, "");
+
 export const ENV = {
-  API_BASE_URL:
-    `${import.meta.env.VITE_API_BASE_URL}/api/v1` ||
-    "https://authentication-system-one-orpin.vercel.app/register/api/v1",
+  API_BASE_URL: `${cleanBaseUrl}/api/v1`,
   IS_DEV: import.meta.env.DEV,
 };
