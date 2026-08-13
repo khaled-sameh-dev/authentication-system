5f989332622723b900b8ca427fab316efa249b2b
unknown <khaledsameh358@gmail.com>
2026-08-13 15:47:44 +0300

    modify env

--- client/src/config/env.config.ts
+++ client/src/config/env.config.ts
@@ -1,7 +1,16 @@
-const rawBaseUrl =
-  import.meta.env.VITE_API_BASE_URL ||
-  "https://authentication-system-production-40b5.up.railway.app";
+const getBaseUrl = () => {
+  const url =
+    import.meta.env.VITE_API_BASE_URL ||
+    "https://authentication-system-production-40b5.up.railway.app";
 
+  if (!/^https?:\/\//i.test(url)) {
+    return `https://${url}`;
+  }
+
+  return url;
+};
+
+const rawBaseUrl = getBaseUrl();
 const cleanBaseUrl = rawBaseUrl.replace(/\/$/, "");
 
 export const ENV = {
